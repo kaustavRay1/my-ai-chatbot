b03cee5ce893b7c078987c78d51e21ebafe7703b
kaustavRay1 <kaustavray2004@gmail.com>
2024-07-18 18:57:29 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [question,setQuestion] =useState("");
+  const [answer,setAnswer] =useState("");
+  async function generateSolution() {
+    console.log("loading..");
+     const response= await axios({
+      url:"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyBJ818OEIcezhzOQ5gSRXC3uGmpQW43zro", 
+      method:"post",
+      data:{contents:[ 
+        {parts :[
+          {text : question },],},],},
+
+    })
+    setAnswer(response['data']['candidates'][0]['content']['parts'][0]['text']);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <div style={{justifyContent:"center", alignItems:"center", display:"flex", flexDirection:"column"}}>
+   <h1>AI Chatbot</h1>
+   <textarea  cols={50} rows={10} value={question} onChange={e =>setQuestion(e.target.value)}/><br/>
+   <button onClick={generateSolution}>Generate Answer</button><br/>
+   <textarea  cols={50} rows={10} value={answer}/>
+   </div>
+   </>
   );
 }
 
